18e58feb29dfae6ac362ccb9e005f7dbc29242c7
Johna N. Morris <johna.n.morris@gmail.com>
2016-08-25 15:06:04 -0700

    Partial implementation of withdraw_using_check. TODO: account for negative max overdraw.

--- checking_account.rb
+++ checking_account.rb
@@ -19,7 +19,28 @@ class CheckingAccount < Bank::Account
 	end
 
 	def withdraw_using_check(amount)
-
+		# The input amount gets taken out of the account as a
+		# result of a check withdrawal. Returns the updated
+		# account balance.
+		# Allows the account to go into overdraft up to -$10
+		# but not any lower
+		# The user is allowed three free check uses in one month,
+		# but any subsequent use adds a $2 transaction fee
+		# reset_checks: Resets the number of checks used to zero
+		fee = 200
+		if amount <= 0
+			puts "Invalid amount."
+		else
+			@checks += 1
+			if @checks <= 3
+				@balance -= amount
+				puts @checks
+			else
+				@balance -= (amount + fee)
+				puts @checks
+			end
+		end
+		return @balance
 	end
 
 	def reset_checks
