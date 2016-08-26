bdc88c4b0b9c62c658d98035c3fa083462bd4182
Johna N. Morris <johna.n.morris@gmail.com>
2016-08-26 15:16:57 -0700

    added self.all_with_owners and add_owner methods

--- bank.rb
+++ bank.rb
@@ -12,7 +12,6 @@ module Bank
 			unless @balance >= self.class::MIN_BALANCE
 				raise ArgumentError.new("Initial balance is too low.")
 			end
-#			@owner = add_owner(owner)
 		end
 
 		def withdraw(amount)
@@ -39,52 +38,52 @@ module Bank
 			# in the CSV.
 			accounts = []
 			CSV.read("/users/johnamorris/ada/project-forks/BankAccounts/support/accounts.csv").each do |line|
-				id = line[0].to_i
+				acct_id = line[0].to_i
 				balance = line[1].to_i
 				open_date = line[2]
-				account = Account.new(id, balance, open_date)
+				account = Account.new(acct_id, balance, open_date)
 				accounts << account
 			end
 			return accounts
 		end
 
-		def self.find(csv_id)
+		def add_owner(acct_id, acct_owner)
+			a = Bank::Account.find(acct_id)
+			@owner = acct_owner
+			puts "#{acct_id} : #{acct_owner}"
+			return @owner
+		end
+
+		def self.find(id)
 			#returns an instance of Account where
 			# the value of the id field in the CSV
 			# matches the passed parameter
 			acct_db = self.all
 			acct_db.each do |account|
-				if account.id == csv_id
+				if account.id == id
 					return account
 				end
 			end
 		end
 
-# 		I can't get this to work.
-#		Note to self: change local variable name from owner to
-#		something else -- attr_reader are affecting this!
-#
-#		def self.all_with_owners
-#			accounts = self.all
-#			account_hash = {}
-#
-#			CSV.read("/users/johnamorris/ada/project-forks/BankAccounts/support/account_owners.csv").each do |line|
-#				puts "Account id: #{line[0]}"
-#				puts "Owner id : #{line[1]}"
-#				account_id = line[0].to_i
-#				owner_id = line[1].to_i
-#				account_hash[account_id] = owner_id
-#			end
-#
-#			accounts.each do |account|
-#				a = account.id
-#				puts "Account id : #{a}"
-#				a.add_owner(owner)
-#			end
-#		end
-#
-#		def add_owner(owner)
-#		end
+		def self.all_with_owners
+			account_hash = {}
+			accounts = []
+
+			CSV.read("/users/johnamorris/ada/project-forks/BankAccounts/support/account_owners.csv").each do |line|
+				account_id = line[0].to_i
+				owner_id = line[1].to_i
+				account_hash[account_id] = owner_id
+			end
+
+			account_hash.each do |acct, own|
+				a = self.find(acct)
+				o = Owner.find(own)
+				a.add_owner(acct, o)
+				accounts << a
+			end
+			return accounts
+		end
 	end
 
 	class Owner
@@ -102,13 +101,13 @@ module Bank
 		def self.all
 			owners = []
 			CSV.read("/users/johnamorris/ada/project-forks/BankAccounts/support/owners.csv").each do |line|
-				id = line[0].to_i
+				owner_id = line[0].to_i
 				last_name = line[1]
 				first_name = line[2]
 				street = line[3]
 				city = line[4]
 				state = line[5]
-				owner = Owner.new(id, last_name, first_name, street, city, state)
+				owner = Owner.new(owner_id, last_name, first_name, street, city, state)
 				owners << owner
 			end
 			return owners
@@ -119,9 +118,9 @@ module Bank
 			# the value of the id field in the CSV
 			# matches the passed parameter
 			owner_db = self.all
-			owner_db.each do |acct_owner|
-				if owner.id == id
-					return acct_owner
+			owner_db.each do |acct|
+				if acct.id == id
+					return acct
 				end
 			end
 		end
